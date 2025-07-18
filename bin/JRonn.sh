959e3ecda223116ce118b06b8bd2470769036c4e
Stuart MacGowan <smacgowan@dundee.ac.uk>
2025-07-18 15:12:25 +0100

    Add JRonn script to execute JRONN and process output with jalview_parser

---
+++ bin/JRonn.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+java -jar $SLIVKA_HOME/bin/bj3.0.4p-jronn.jar $*
+
+returncode=$?
+if [ $returncode -ne 0 ]; then
+    exit $returncode
+fi
+for arg in $*; do
+    if [[ ${arg} == -o=* ]]; then
+        outputFile=${arg:3}
+        break
+    fi
+done
+python $SLIVKA_HOME/scripts/jalview_parser.py jronn --input ${outputFile} --annot jronn.jvannot
